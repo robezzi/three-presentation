947ef6cf5723b5f99205f45a3d770e29a1e8db2b
Mikhail Kochelev <mikhailKochel@outlook.com>
2025-09-08 12:23:08 +0700

    .

--- eslint.config.js
+++ eslint.config.js
@@ -5,29 +5,29 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'prefer-const': 'error',
-      'react-hooks/rules-of-hooks': 'error',
-      'no-console': 'warn',
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
-    },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
+	{ ignores: ['dist'] },
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs['recommended-latest'],
+			reactRefresh.configs.vite,
+		],
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': 'warn',
+			'@typescript-eslint/explicit-function-return-type': 'warn',
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'prefer-const': 'error',
+			'react-hooks/rules-of-hooks': 'error',
+			'no-console': 'warn',
+			quotes: ['error', 'single'],
+			semi: ['error', 'always'],
+		},
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+	},
 ]);
